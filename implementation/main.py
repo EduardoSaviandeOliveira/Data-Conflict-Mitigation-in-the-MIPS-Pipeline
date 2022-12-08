de1372882522c398387829fd86971c0dcf3a3e84
Eduardo-Savian-de-Oliveira <eduardosaviandeoliveira@proton.me>
2022-12-08 15:39:03 -0300

    Added one more test

--- implementation/main.py
+++ implementation/main.py
@@ -3,12 +3,12 @@ from nops import insertion_of_nops
 from reorderer import instruction_reorderer
 
 if __name__ == '__main__':
-    file = 'test_reo'
+    file = 'test1'
     instructions = read_file(file)
-    instructions_nops = insertion_of_nops(instructions)
+    # instructions_nops = insertion_of_nops(instructions)
+    # write_file_nops(file, instructions_nops)
+
+    instructions_reorder = instruction_reorderer(instructions)
+    write_file_reorder(file, instructions_reorder)
+    instructions_nops = insertion_of_nops(instructions_reorder)
     write_file_nops(file, instructions_nops)
-    
-    #instructions_reorder = instruction_reorderer(instructions)
-    #write_file_reorder(file, instructions_reorder)
-    #instructions_nops = insertion_of_nops(instructions_reorder)
-    #write_file_nops(file, instructions_nops)
